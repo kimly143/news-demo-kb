fca2cb27f5e21b3612bcccd06e05709b22abe695
Kim Ly Buck <kim.3993@gmail.com>
2020-02-23 19:20:00 -0600

    create tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,23 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+//create tab
+const createTab = (topic) => {
+    const t = document.createElement('div');
+    t.classList.add('tab');
+    t.textContent = topic;
+    return t;
+}
+
+//axios
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then((response) => {
+        //console.log(response.data);
+        (response.data.topics).forEach(tp => {
+            const topic = createTab(tp);
+            document.querySelector('.topics').appendChild(createTab(tp));
+        });
+    })
+    .catch((error) => {console.error(error)});
+
