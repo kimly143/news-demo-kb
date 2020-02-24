bbe20526a2eda772ecf896a0362df8c9f92b2bd1
Kim Ly Buck <kim.3993@gmail.com>
2020-02-23 18:56:50 -0600

    added header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+//function Header() {}
+
+const createHeader = () => {
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const d = document.createElement('span');
+    d.classList.add('date');
+    d.textContent = 'SMARCH 28, 2019';
+    header.appendChild(d);
+
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Lambda Times';
+    header.appendChild(h1);
+
+    const t = document.createElement('span');
+    t.classList.add('temp');
+    t.textContent = '98°';
+    header.appendChild(t);
+    
+    return header;
+}
+document.querySelector('.header-container').appendChild(createHeader());
